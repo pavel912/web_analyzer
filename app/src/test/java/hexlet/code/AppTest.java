737aefb809bb93332c4e61fe55a8a654f457e832
pavel912 <gta4thebest@mail.ru>
2023-02-17 22:08:39 +0300

    add sample test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,11 @@
+package hexlet.code;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
