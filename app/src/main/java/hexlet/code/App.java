f3cbf36d99491cc98047aa78aa0aea00c4121a92
pavel912 <gta4thebest@mail.ru>
2023-02-18 15:29:53 +0300

    add javalin app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,36 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
+import hexlet.code.controllers.RootController;
+
 public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "5000");
+        return Integer.parseInt(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", RootController.welcome);
+    }
+
+    public static Javalin getApp() {
+
+        Javalin app = Javalin.create(config -> {
+            config.plugins.enableDevLogging();
+        });
+
+        addRoutes(app);
+
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+
+        return app;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Javalin app = getApp();
+        app.start(getPort());
     }
 }
